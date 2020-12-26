bf624a8675408fad91c3484398e04f53f43455f2
your namemaher <maher98>
2020-12-26 20:27:00 +0200

    Add project files.

---
+++ Core/Entities/Address.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Entities
+{
+    public class Address : EntitiesBase
+    {
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }
+    }
+}
---
+++ Core/Entities/Owner.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Entities
+{
+    public class Owner : EntitiesBase
+    {
+        public string Name { get; set; }
+        public string Profile { get; set; }
+        public string Picture { get; set; }
+        public Address address { get; set; }
+    }
+}
